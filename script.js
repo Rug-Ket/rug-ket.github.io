339f6412e304ed434ec351420a5dbeed34beb6be
Rug-Ket <angeltorrespardillo@gmail.com>
2024-09-13 10:17:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,80 @@
 var Selection;
 var Selection2;
+
 function Rock(){
     Selection = 0;
     aleatorio = Math.floor(Math.random()*3);
     if (aleatorio == 0) {
         Selection2 = "Piedra";
-        alert("Empate");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Empate tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 1) {
         Selection2 = "Papel";
-        alert("Derrota");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Derrota tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 2) {
         Selection2 = "Tijeras";
-        alert("Victoria");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Victoria tu oponete tiene " + Selection2;
+        result.showModal();
     }
-    alert("Tu oponente tiene " + Selection2);
 };
+
 function Paper(){
     Selection = 1;
     aleatorio = Math.floor(Math.random()*3);
     if (aleatorio == 0) {
         Selection2 = "Piedra";
-        alert("Victoria");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Victoria tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 1) {
         Selection2 = "Papel";
-        alert("Empate");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Empate tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 2) {
         Selection2 = "Tijeras";
-        alert("Derrota");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Derrota tu oponete tiene " + Selection2;
+        result.showModal();
     }
-    alert("Tu oponente tiene " + Selection2);
 };
+
 function Scissors(){
     Selection = 2;
     aleatorio = Math.floor(Math.random()*3);
     if (aleatorio == 0) {
         Selection2 = "Piedra";
-        alert("Derrota");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Derrota tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 1) {
         Selection2 = "Papel";
-        alert("Victoria");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Victoria tu oponete tiene " + Selection2;
+        result.showModal();
     }
     else if (aleatorio == 2) {
         Selection2 = "Tijeras";
-        alert("Empate");
+        let result = document.getElementById("resultado");
+        let frase = document.getElementById("frase");
+        frase.innerHTML = "Empate tu oponete tiene " + Selection2;
+        result.showModal();
     }
-    alert("Tu oponente tiene " + Selection2);
 };
